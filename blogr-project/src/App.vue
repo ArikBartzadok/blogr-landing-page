67507658a391c010dbe86f2a1e7201f19f11859c
Diogo Ferreira <arikbartzadok@gmail.com>
2021-05-29 01:59:46 -0300

    reestructuring and configs to vue project

---
+++ blogr-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+		<TheHeader />
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader"
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ blogr-project/src/components/TheHeader/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<header>
+		<nav>
+			<div class="logo">
+				<p>logo</p>
+			</div>
+			<div class="menu">
+				<a href="">aaaaa</a>
+				<a href="">aaaaa</a>
+				<a href="">aaaaa</a>
+			</div>
+			<div class="options">
+				<button>xxx</button>
+				<button>xxx</button>
+			</div>
+		</nav>
+		<section>
+			<span>teste tittulo</span>
+			<span>teste desc</span>
+			<div class="option">
+				<button>xxx</button>
+				<button>xxx</button>
+			</div>
+		</section>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "TheHeader"
+}
+</script>
+
+<style lang="scss" src="./style.scss"></style>
